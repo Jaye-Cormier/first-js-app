f28b71073f97720afb602e808cf11e7df6005c2a
Jaye-Cormier <103283655+Jaye-Cormier@users.noreply.github.com>
2022-05-16 15:28:01 -0400

    Modal addition to app
    added modal css and related styling, updated index title, introduced all modal functions in script file

--- js/scripts.js
+++ js/scripts.js
@@ -4,6 +4,75 @@ let pokemonRepository = (function() {
 let pokemonList = [];
 let apiUrl = 'https://pokeapi.co/api/v2/pokemon/?limit=151';
 
+// initiate modal info
+let modalContainer = document.querySelector('#modal-container');
+
+function showModal(pokemon) {
+  let modal = document.createElement('div');
+  modal.classList.add('modal');
+
+  // clear all existing modal content
+    modalContainer.innerHTML = '';
+
+  let closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+  let titleElement = document.createElement('h2');
+  titleElement.classList.add('title-element');
+  titleElement.innerText = pokemon.name.toUpperCase();
+
+  let pokemonHeight = document.createElement('p');
+  pokemonHeight.classList.add('pokemon-height');
+  pokemonHeight.innerText = "Height: " + pokemon.height;
+
+  let imageContainer = document.createElement('div');
+  imageContainer.classList.add('img-container', 'grid__item');
+
+  let imageFront = document.createElement('img');
+  imageFront.classList.add('image-front');
+  imageFront.src = pokemon.imageUrlFront;
+
+  let pokemonWeight = document.createElement('p');
+  pokemonWeight.classList.add('pokemon-weight');
+  pokemonWeight.innerText = 'Weight: ' + pokemon.weight;
+
+  let imageContainerShiny = document.createElement('div');
+  imageContainerShiny.classList.add('img-container-shiny', 'grid__item');
+
+  let imageElementShiny = document.createElement('img');
+  imageElementShiny.classList.add('image-shiny');
+  imageElementShiny.src = pokemon.imageUrlShiny;
+
+
+  modal.appendChild(closeButtonElement);
+  modal.appendChild(titleElement);
+  modal.appendChild(pokemonHeight);
+  modal.appendChild(pokemonWeight);
+  modal.appendChild(imageContainer);
+  modal.appendChild(imageContainerShiny);
+  imageContainer.appendChild(imageFront);
+  imageContainerShiny.appendChild(imageElementShiny);
+
+
+  modalContainer.appendChild(modal);
+
+  modalContainer.classList.add('is-visible');
+
+}
+
+  let dialogPromiseReject; // This can be set later by showDialog
+
+  function hideModal() {
+
+    modalContainer.classList.remove('is-visible');
+
+    if(dialogPromiseReject) {
+      dialogPromiseReject();
+      dialogPromiseReject = null;
+    }
+  }
 
 function getAll(){
   return pokemonList;
@@ -17,7 +86,7 @@ function addListItem(pokemon) {
   let pokemonList = document.querySelector('.pokemon-list');
   let listPokemon = document.createElement('li');
   let button = document.createElement('button');
-  button.innerText = pokemon.name;
+  button.innerText = pokemon.name.toUpperCase();
   button.classList.add("button-class");
   listPokemon.appendChild(button);
   pokemonList.appendChild(listPokemon);
@@ -48,9 +117,11 @@ function loadDetails(item) {
   return fetch(url).then(function (response) {
     return response.json();
   }).then(function (details) {
-    item.imageUrl = details.sprites.front_default;
+    item.imageUrlFront = details.sprites.front_default;
+    item.imageUrlShiny = details.sprites.front_shiny;
     item.height = details.height;
-    item.types = details.types[0].type.name;
+    item.weight = details.weight;
+    item.types = details.types;
   }).catch(function (e) {
     console.error(e);
   });
@@ -58,9 +129,26 @@ function loadDetails(item) {
 
 function showDetails(pokemon) {
   loadDetails(pokemon).then(function(){
-    console.log(pokemon);
+    showModal(pokemon);
   });
 }
+
+window.addEventListener('keydown', (e) => {
+
+    if (e.key === 'Escape' && modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  modalContainer.addEventListener('click', (e) => {
+      // Since this is also triggered when clicking INSIDE the modal
+      // We only want to close if the user clicks directly on the overlay
+      let target = e.target;
+      if (target === modalContainer) {
+        hideModal();
+      }
+    });
+
 return {
   getAll: getAll,
   add: add,
